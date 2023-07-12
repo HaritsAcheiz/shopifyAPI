60395e6a35c6f0f5eecb982c13749fdc13f5c460
Muhammad Harits R <harits.muhammad.only@gmail.com>
2023-07-13 05:08:53 +0700

    success get response from shopify api

--- main.py
+++ main.py
@@ -21,21 +21,31 @@ class ShopifyApp:
     def main(self):
         client = self.create_session()
         data = {
+
             "query": '''{
-                products(first: 3) {
-                    edges {
-                        node {
-                            id
-                            title
-                        }
-                    }
+                shop{
+                    name
                 }
             }'''
+
+            # "query": '''{
+            #     products(first: 3) {
+            #         edges {
+            #             node {
+            #                 id
+            #                 title
+            #             }
+            #         }
+            #     }
+            # }'''
+            # "variables": '''{
+            #
+            # }'''
+
         }
         response = client.post(f'https://{self.store_name}.myshopify.com/admin/api/2023-07/graphql.json', json=data)
         print(response)
         print(response.json())
-        print(response.headers)
 
 
 if __name__ == '__main__':
