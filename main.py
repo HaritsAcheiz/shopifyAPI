6804da0623a593361571f319193a67415c5b4335
Muhammad Harits R <harits.muhammad.only@gmail.com>
2023-07-17 16:33:23 +0700

    create product with image on shopify completed, try to set publication

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import json
 import os
 import pandas as pd
 from urllib.parse import urljoin
+from datetime import datetime
+
 
 @dataclass
 class ShopifyApp:
@@ -134,34 +136,59 @@ class ShopifyApp:
         return response.json()
 
     def create_products(self, client, staged_target):
+        print('Creating products...')
+        # mutation = '''
+        #             mutation ($stagedUploadPath: String!){
+        #                 bulkOperationRunMutation(
+        #                     mutation: "mutation call($input: ProductInput!)
+        #                     { productCreate(input: $input) { product {id title variants(first: 10) {edges {node {id title inventoryQuantity }}}} userErrors { message field } } }",
+        #                     stagedUploadPath: $stagedUploadPath
+        #                 )
+        #                 {
+        #                     bulkOperation {
+        #                         id
+        #                         url
+        #                         status
+        #                     }
+        #                     userErrors {
+        #                         message
+        #                         field
+        #                     }
+        #                 }
+        #             }
+        #             '''
+
         mutation = '''
-                    mutation {
-                        bulkOperationRunMutation(
-                            mutation: "mutation call($input: ProductInput!) { productCreate(input: $input) { product {id title variants(first: 10) {edges {node {id title inventoryQuantity }}}} userErrors { message field } } }",
-                            stagedUploadPath: $stagedUploadPath
-                        )
-                        {
-                            bulkOperation {
-                                id
-                                url
-                                status
+                            mutation ($stagedUploadPath: String!){
+                                bulkOperationRunMutation(
+                                    mutation: "mutation call($input: ProductInput!, $media: [CreateMediaInput!])
+                                    { productCreate(input: $input, media: $media) { product {id title variants(first: 10) {edges {node {id title inventoryQuantity }}}} userErrors { message field } } }",
+                                    stagedUploadPath: $stagedUploadPath
+                                )
+                                {
+                                    bulkOperation {
+                                        id
+                                        url
+                                        status
+                                    }
+                                    userErrors {
+                                        message
+                                        field
+                                    }
+                                }
                             }
-                            userErrors {
-                                message
-                                field
-                            }
-                        }
-                    }
-                    '''
+                            '''
 
         variables = {
-            "stagedUploadPath": staged_target['data']['stagedUploadsCreate']['stagedTargets'][0]['parameters'][4]['value']
+            "stagedUploadPath": staged_target['data']['stagedUploadsCreate']['stagedTargets'][0]['parameters'][3]['value']
         }
 
         response = client.post(f'https://{self.store_name}.myshopify.com/admin/api/2023-07/graphql.json',
                                json={"query": mutation, "variables": variables})
+
         print(response)
         print(response.json())
+        print(response.request.headers)
         print('')
 
     def csv_to_jsonl(self, csv_filename, jsonl_filename):
@@ -182,15 +209,15 @@ class ShopifyApp:
             files[f"{parameter['name']}"] = (None, parameter['value'])
         files['file'] = open(jsonl_path, 'rb')
 
-        with httpx.Client() as sess:
-            response = sess.post(url, files=files)
+        # with httpx.Client(timeout=None, follow_redirects=True) as sess:
+        response = httpx.post(url, files=files)
 
         print(response)
         print(response.content)
         print('')
 
     def import_bulk_data(self, client, csv_filename, jsonl_filename):
-        self.csv_to_jsonl(csv_filename=csv_filename, jsonl_filename=jsonl_filename)
+        # self.csv_to_jsonl(csv_filename=csv_filename, jsonl_filename=jsonl_filename)
         staged_target = self.generate_staged_target(client)
         self.upload_jsonl(staged_target=staged_target, jsonl_path=jsonl_filename)
         self.create_products(client, staged_target=staged_target)
@@ -198,9 +225,10 @@ class ShopifyApp:
 if __name__ == '__main__':
     s = ShopifyApp()
     client = s.create_session()
-    s.query_shop(client)
-    s.query_product(client)
-    s.create_product(client)
-    s.csv_to_jsonl(csv_filename='result.csv', jsonl_filename='test2.jsonl')
-    staged_target = s.generate_staged_target(client)
-    s.upload_jsonl(staged_target=staged_target, jsonl_path="D:/Naru/shopifyAPI/bulk_op_vars.jsonl")
+    # s.query_shop(client)
+    # s.query_product(client)
+    # s.create_product(client)
+    # s.csv_to_jsonl(csv_filename='result.csv', jsonl_filename='test2.jsonl')
+    # staged_target = s.generate_staged_target(client)
+    # s.upload_jsonl(staged_target=staged_target, jsonl_path="D:/Naru/shopifyAPI/bulk_op_vars.jsonl")
+    s.import_bulk_data(client=client, csv_filename='result.csv', jsonl_filename='bulk_op_vars.jsonl')
